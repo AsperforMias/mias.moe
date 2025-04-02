d6a0f360541958a1c2c2f9180970c9c8d028ff86
AsperforMias <asperformias000@outlook.com>
2025-04-03 01:53:26 +0800

    Updata: modify the consts.ts,change the info.

--- src/consts.ts
+++ src/consts.ts
@@ -1,15 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'AsperforMias Blog';
+export const SITE_TITLE = '我追寻最初梦的光点，只为让噩梦迎来终焉 | AsperforMias-Blog';
 
-export const SITE_DESCRIPTION = 'AsperforMias\'s blog,built with SolidJS and Astro ';
+export const SITE_DESCRIPTION = 'A simple blog of AsperforMias,built with SolidJS and Astro ';
 
 export const FAVICON_SRC = '/favicon.svg';
 
 export const AVATAR_SRC = '/avatar.avif';
 
-export const BIO = "Hi~这里是米娜，一个平庸的大飞舞,什么都不会！QAQ";
+export const BIO = "Hi~ I'm Mia.👋 A developer actively working towards the full-stack realm, hoping to eventually become a software architect! I've also dabbled a little bit in cybersecurity and penetration testing on the side(?)";
 
 export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'lightGlass'
 
