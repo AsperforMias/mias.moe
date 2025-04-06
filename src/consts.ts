3a267cd386666f98b3a5e57d4da7c08e8f819530
AsperforMias <asperformias000@outlook.com>
2025-04-06 16:14:33 +0800

    Updata:change const SITE_title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '『我追寻最初梦的光点，只为让噩梦迎来终焉。』| Mias-Blog';
+export const SITE_TITLE = '我追寻最初梦的光点，只为让噩梦迎来终焉 | Mias-Blog';
 
 export const SITE_NAME = 'AsperforMias\' Blog'
 
